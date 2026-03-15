a819ae767cf9d87681f73c1ce42f37b6f3ba8af3
ishitaprasad <ishitaprasad421@gmail.com>
2026-03-15 16:58:44 +0530

    Fix backend environment

--- backend/models.py
+++ backend/models.py
@@ -1,7 +1,9 @@
-import tensorflow as tf
+from tensorflow.keras.models import load_model
 import joblib
+import tensorflow as tf
 
-cycle_model = tf.keras.models.load_model("cycle_model.h5")
+cycle_model = tf.saved_model.load("cycle_model")
+
 
 feature_scaler = joblib.load("feature_scaler.pkl")
 target_scaler = joblib.load("target_scaler.pkl")
